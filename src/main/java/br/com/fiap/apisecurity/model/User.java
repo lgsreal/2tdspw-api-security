f83e38054c8377f48446cab1c627833a5e8a1928
Luiz Gustavo S. Real <lgsreal2@gmail.com>
2025-04-01 23:08:52 -0300

    add table name

--- src/main/java/br/com/fiap/apisecurity/model/User.java
+++ src/main/java/br/com/fiap/apisecurity/model/User.java
@@ -1,9 +1,6 @@
 package br.com.fiap.apisecurity.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
